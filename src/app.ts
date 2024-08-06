f4a9d8d434ed0705f1b6028243148466417a1e5e
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-06 08:16:50 -0500

    feat(Database): Creating a class to connect to mongo

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { MongoDatabase } from "./data/mongo/init"
 import { AppRouter } from "./presentation/router"
 import { Server } from "./presentation/server"
 
@@ -5,6 +6,11 @@ import { Server } from "./presentation/server"
   const server = new Server({
     port: 3000,
     routes: AppRouter.routes
+  })
+
+  await MongoDatabase.connect({
+    dbName: '',
+    mongoUrl: ''
   })
 
   server.start();
