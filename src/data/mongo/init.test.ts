2a34f6579a33a55e5671abee7013795e08b609ad
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-06 11:26:54 -0500

    feat(testing): Testing on db connection

---
+++ src/data/mongo/init.test.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { MongoDatabase } from "./init";
+
+
+describe("init MongoDB", () => {
+  afterAll(() => {
+    mongoose.connection.close();
+  });
+
+  it("should connect to MongoDB", async () => {
+
+    const connected = await MongoDatabase.connect({
+      dbName: process.env.MONGO_DB_NAME!,
+      mongoUrl: process.env.MONGO_URL!,
+    });
+
+    expect(connected).toBe(true);
+  });
+
+  it("should throw an error", async () => {
+    try {
+      const connected = await MongoDatabase.connect({
+        dbName: process.env.MONGO_DB_NAME!,
+        mongoUrl: "mongodb://nandier:123456789@localhoasdsast:27017",
+      });
+      expect(true).toBe(false);
+    } catch (error) {}
+  });
+});
